e40b3b3a2fca5c34278719e61c9aa6f45c888ced
pjk0007 <44570338+pjk0007@users.noreply.github.com>
2021-11-27 10:29:52 +0900

    clock

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector(".clock");
+
+function getClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
